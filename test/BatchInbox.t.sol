35a6183e5fd59c59cadce2813056c9f4cdb5d6b0
zhiqiangxu <652732310@qq.com>
2024-03-31 14:36:49 +0800

    add deposit/withdraw

--- test/BatchInbox.t.sol
+++ test/BatchInbox.t.sol
@@ -11,8 +11,40 @@ contract BatchInboxTest is Test {
         batchInbox = new BatchInbox(0x804C520d3c084C805E37A35E90057Ac32831F96f);
     }
 
-    function test_Increment() public {
-        
+    function testDepositWithdrawOK() public {
+        // Give the test contract some ether
+        vm.deal(address(this), 1000 ether);
+
+        // deposit 1 ether for target
+        address target = address(111);
+        uint256 amount = 1 ether;
+        uint256 balanceBefore = target.balance;
+        assertEq(batchInbox.balances(target), 0);
+        batchInbox.deposit{value: amount}(target);
+        assertEq(batchInbox.balances(target), amount);
+
+        // withdraw 1 ether from target, to target
+        vm.prank(target);
+        batchInbox.withdraw(target, amount);
+        uint256 balanceAfter = target.balance;
+
+        assertEq(balanceBefore + amount, balanceAfter);
     }
 
+    function testDepositWithdrawNG() public {
+        // Give the test contract some ether
+        vm.deal(address(this), 1000 ether);
+
+        // deposit 1 ether for target
+        address target = address(111);
+        uint256 amount = 1 ether;
+        assertEq(batchInbox.balances(target), 0);
+        batchInbox.deposit{value: amount}(target);
+        assertEq(batchInbox.balances(target), amount);
+
+        // withdraw 2 ether from target, to target
+        vm.prank(target);
+        vm.expectRevert(BatchInbox.BalanceNotEnough.selector);
+        batchInbox.withdraw(target, 2 ether);
+    }
 }
